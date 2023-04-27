d26b34e159a449e1f627cb7587976289327261d0
Yekta <yektakayabasi@gmail.com>
2023-04-27 12:55:28 +0300

    v3

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -5,6 +5,8 @@ public class AtmProject {
         //version1
         System.out.println("Java is easy");
         //version2
+        System.out.println("Learn Java earn money ");
+        //version3
 
 
 
